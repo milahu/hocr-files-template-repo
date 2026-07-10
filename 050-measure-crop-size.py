eaa2e12ffbad3df621a92923af195e99cd0a26dc
Milan Hauth <milahu@milahu.duckdns.org>
2026-07-10 11:41:21 +0200

    lowthresh=0.3

--- 050-measure-crop-size.py
+++ 050-measure-crop-size.py
@@ -23,8 +23,13 @@ crop_even_box = (0, 0, crop_size[0], crop_size[1])
 # from black and white areas in text
 # but too much leveling causes too much loss in contrast
 # in darkgray and lightgray areas in grayscale graphics
+# lowthresh=0.3 is necessary to remove dither from black areas
+# lowthresh and highthresh should be symmetrical (lowthresh + highthresh == 1)
+# so contours are preserved
+# todo: use different leveling values for text and graphics
+# for graphics, we want lowthresh=0.05
+# to preserve darkgray and lightgray areas in grayscale graphics
 do_level = True
-lowthresh = 0.05 # about 15/255 in GIMP
-highthresh = 0.95 # about 240/255 in GIMP
-# level = f"{lowthresh}x{highthresh}%"
-lowthresh = 0.2; highthresh = 0.8
+# lowthresh, highthresh = 0.05, 0.95
+# lowthresh, highthresh = 0.2, 0.8
+lowthresh, highthresh = 0.3, 0.7
