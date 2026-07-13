4b8c99fc6391b483c03ba5fd71f63eeadc031dde
Milan Hauth <milahu@milahu.duckdns.org>
2026-07-13 11:25:48 +0200

    move config to 000-config.py

--- 000-config.py
+++ 000-config.py
@@ -4,19 +4,38 @@ num_pages = 592
 
 color_pages = []
 
+# TODO rename all mm sizes to scan_x_mm etc
+
+max_scan_x, max_scan_y = 215.88, 355.567 # maximum
+
 # NOTE this should be the full page size in millimeters
 # full size = before the book binding was removed
 # this size is used
 # 1. in 040-scan-pages.py
 # 2. in 065-remove-page-borders.py to restore the original page size
 # scan_x, scan_y = 210, 297 # DIN A4
-# scan_x, scan_y = 215.88, 355.567 # maximum
 scan_x, scan_y = 147, 231
+scan_x, scan_y = 156, 210
+scan_x, scan_y = 210, 156 # rotate 90
+
+# scan_x, scan_y = max_scan_x, max_scan_y # maximum
+
+# this is True for most book pages
+# in reading order, the page width is smaller than the page height
+# orientation_is_portrait = False
+orientation_is_portrait = True
+
+assert scan_x <= max_scan_x, f"scan_x is out of range: {scan_x} > {max_scan_x}"
+assert scan_y <= max_scan_y, f"scan_y is out of range: {scan_y} > {max_scan_y}"
 
 # add margin for 065-remove-page-borders.py
 scan_margin = 10
-scan_x = scan_x + scan_margin
-scan_y = scan_y + scan_margin
+
+# no! do this only in 040-scan-pages.py
+# scan_x = scan_x + scan_margin
+# scan_y = scan_y + scan_margin
+
+
 
 # 300 dpi -> 12 MB
 # 600 dpi -> 50 MB
@@ -44,5 +63,48 @@ image_format = "jpg"
 
 # scan_mode = "24bit Color[Fast]"
 scan_mode = "True Gray"
+scan_mode = "24bit Color[Fast]"
 
+# "center aligned" is not working: scanimage failed with returncode -11
+# scan_source = "Automatic Document Feeder(center aligned,Duplex)"
 scan_source = "Automatic Document Feeder(left aligned,Duplex)"
+
+
+
+# Config for 060-rotate-crop.py
+
+# --- Rotation settings ---
+do_rotate = True
+# rotate_odd, rotate_even = 90, 270
+# book binding side = scanner top side
+rotate_odd, rotate_even = 270, 90
+
+
+
+# --- Crop settings ---
+do_crop = False
+crop_size = (1580, 2480)
+crop_x = 168
+# x1, y1, x2, y2 = crop_box
+crop_odd_box = (crop_x, 0, crop_x + crop_size[0], crop_size[1])
+crop_even_box = (0, 0, crop_size[0], crop_size[1])
+
+
+
+# Config for 0663-level.py
+
+# --- Level / brightness normalization ---
+# leveling is useful to remove noise
+# from black and white areas in text
+# but too much leveling causes too much loss in contrast
+# in darkgray and lightgray areas in grayscale graphics
+# lowthresh=0.3 is necessary to remove dither from black areas
+# lowthresh and highthresh should be symmetrical (lowthresh + highthresh == 1)
+# so contours are preserved
+# todo: use different leveling values for text and graphics
+# for graphics, we want lowthresh=0.05
+# to preserve darkgray and lightgray areas in grayscale graphics
+do_level = True
+# lowthresh, highthresh = 0.05, 0.95
+# lowthresh, highthresh = 0.2, 0.8
+lowthresh, highthresh = 0.3, 0.7
